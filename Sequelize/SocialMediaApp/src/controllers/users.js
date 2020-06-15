a6d87ed051c0f7f8c89b28307b45a5eeae129aba
Sanyam Aggarwal <i-sanyam>
2020-06-15 12:16:51 +0530

    Random user creation endpoints done

---
+++ Sequelize/SocialMediaApp/src/controllers/users.js
@@ -0,0 +1,18 @@
+const { randomUsernameGen } = require("../utils/username");
+const { Users } = require("../db/models");
+async function createAnnonUser() {
+	const user = await Users.create({ username: randomUsernameGen() });
+	return user;
+}
+
+/* test command */
+/*
+ async function task() {
+	console.log(await createAnnonUser());
+	console.log(await createAnnonUser());
+	console.log(await createAnnonUser());
+	console.log(await createAnnonUser());
+	console.log(await createAnnonUser());
+}
+task();
+*/

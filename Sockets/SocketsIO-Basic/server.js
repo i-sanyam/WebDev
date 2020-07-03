8bf6975113726f0f13a434fa50a3b9ca4927a07a
i-sanyam <sanyam.nitj@gmail.com>
2020-07-03 10:51:26 +0530

    first socket server

--- Sockets/SocketsIO-Basic/server.js
+++ Sockets/SocketsIO-Basic/server.js
@@ -3,8 +3,21 @@ const express = require("express");
 
 const app = express();
 
+const socketio = require("socket.io");
+
 //creates a http server using the express app
 const server = http.createServer(app);
+
+/**
+ * WITHOUT passing the server into the socketio function,
+ * socket.io.js file is not included in our code, and we
+ * lose functionality provided by Socket.IO
+ * TIP: try commenting out "const io = socketio(server)" and then run the server
+ */
+
+const io = socketio(server);
+
+//***************************** */
 
 app.use("/", express.static(__dirname + "/public"));
 
